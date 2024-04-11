abf34e43098df366fadaeaca417bcba567f216a3
Angie R <165759597+arr225@users.noreply.github.com>
2024-04-11 16:47:54 -0700

    Test additional type dropdown.

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ import streamlit as st
 df_vehicles = pd.read_csv('./vehicles_us.csv')
 
 st.header('Vehicles for Sale')
+st.writer('Filter the data below to see the available vehicles by maker')
+
+vehicle_type_choice = df_vehicles['type'].unique()
+
+
 st.write(df_vehicles.head())
+ 
