9781507b105ba7cd7f1709c7598d0fb2393744a0
Angie R <165759597+arr225@users.noreply.github.com>
2024-04-12 00:54:09 -0700

    commit updates

--- main.py
+++ main.py
@@ -15,3 +15,7 @@ selected_menu = st.selectbox('Select Vehicle Type', vehicle_type_choice)
 min_year, max_year = int(df_vehicles['model_year'].min()), int(df_vehicles['model_year'].max())
 
 year_range = st.slider('Choose Model Years', value=(min_year, max_year), min_value=min_year, max_value=max_year)
+
+actual_range = list(range(year_range[0], year_range[1]+1))
+
+df_filtered = df_vehicles[(df_vehicles.type == selected_menu) & (df_vehicles.model_year.isin(list(actual_range)))]
