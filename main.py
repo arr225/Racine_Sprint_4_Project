e813802d8b35457e7e75f12056e9926895f9371b
Angie R <165759597+arr225@users.noreply.github.com>
2024-04-12 12:54:26 -0700

    commit

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import streamlit as st
 
 
 st.header('Vehicles for Sale')
-st.write('Filter the data below to see the available vehicles by maker')
+st.write('Filter the data below to see the available vehicles by vehicle model.')
 
 df_vehicles = pd.read_csv('./vehicles_us.csv')
 
-vehicle_type_choice = df_vehicles['type'].unique()
+vehicle_type_choice = df_vehicles['model'].unique()
 
 selected_menu = st.selectbox('Select Vehicle Type', vehicle_type_choice)
 
@@ -18,6 +18,8 @@ year_range = st.slider('Choose Model Years', value=(min_year, max_year), min_val
 
 actual_range = list(range(year_range[0], year_range[1]+1))
 
-df_filtered = df_vehicles[(df_vehicles.type == selected_menu) & (df_vehicles.model_year.isin(list(actual_range)))]
+df_filtered = df_vehicles[(df_vehicles.model == selected_menu) & (df_vehicles.model_year.isin(list(actual_range)))]
 
 st.write(df_filtered)
+
+st.header('')
